e475741b6f331f4589b72165615f37b44518cf76
Jorge <xalandx@gmail.com>
2020-11-15 13:34:57 +0100

    Añadimos los botones de dificultad y le damos algo de utilidad

---
+++ 05_QuickClick/Assets/_Scripts/DifficultyButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button _button; //Creamos una variable de tipo botón
+    private GameManager gameManager; //creamos una variable de tipo GameManager
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>(); //Buscamos el objeto de tipo GameManager y lo guardamos en la variable gameManager
+        _button = GetComponent<Button>(); //Accedemos a la componente Button y la guardamos en button
+        _button.onClick.AddListener(SetDifficulty); //añadimos una función a los botones, esta es que al hacer click 
+     
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log("El botón" + gameObject.name + " ha sido pulsado.");
+        gameManager.StartGame(); //llamamos a el método .StartGame() que está en el gameManager
+
+    }
+    
+}
--- 05_QuickClick/Assets/_Scripts/GameManager.cs
+++ 05_QuickClick/Assets/_Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using TMPro; //Importamos el paquete textMeshPro
 using UnityEngine;
+using UnityEngine.UI; //Para poder usar los botones
+using UnityEngine.SceneManagement; //Para acceder a el control de las escenas
 
 
 public class GameManager : MonoBehaviour
@@ -25,6 +27,9 @@ public class GameManager : MonoBehaviour
 
     private int _score; //Creamos una variable encargada de tener el valor de la puntuación, haqcemos esto porque vamos a usar un set y get con la variable score
 
+    [SerializeField] private Button restartButton; //Creamos una variable de tipo Button que almacenará un botón, hemos de añadirlo desde el editor
+
+
     private int score //Score será en encargado de comprobar la puntuación que tenemos
     {    
         
@@ -42,15 +47,20 @@ public class GameManager : MonoBehaviour
     }
 
     [SerializeField] private TextMeshProUGUI gameOverText; //Creamos una variable de tipo TextMeshProUGUI para almacenar el texto de el game over, dicho texto lo asignamos mediante Unity
-    
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private GameObject titleScreen; //creamos una variable de tipo GameObject donde desde Unity nos encargamos de arrastrar el panel de la pantalla de título
+
+  
+
+    public void StartGame()
     {
         gameState = GameState.inGame; //Ponemos el estado de el juego en inGame
         StartCoroutine(spawnTarget()); //Empieza la coorutina
         score = 0; //ponemos la puntación a 0
         UpdateScore(score);
         gameOverText.gameObject.SetActive(false);  //como al empezar el juego no queremos que aparezca el texto de gameOver hacemos que esté false
+        restartButton.gameObject.SetActive(false); //Al empezar no queremos ver el botón, podemos ahorrarnos esto desactivando el botón desde Unity y ya está
+        titleScreen.gameObject.SetActive(false); 
     }
 
     IEnumerator spawnTarget() //Como lo que queremos es spawnear los gameObject de forma constante usamos una coorutina
@@ -79,6 +89,13 @@ public class GameManager : MonoBehaviour
     {
         gameState = GameState.gameOver;
         gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true); //Hacemos que se vea el botón
         
     }
+
+     public void RestartGame() //creamos la función que ejecutará el botón cuando es pulsado, recordamos que como se va acceder desde fuera, ha de ser público
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //accedemos el SceneManager y dentro de este cargamos: accedemos a el nombre de la scena actual y la cargamos
+    }
+    
 }
